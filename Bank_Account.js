fb91de252ef8e0dfd8052906d4b043db4fa69093
ShaunFlood <shaunflood@hotmail.co.uk>
2023-06-05 17:19:04 +0100

    Red for first test

--- Bank_Account.js
+++ Bank_Account.js
@@ -1,5 +1,5 @@
 class BankAccount {
-
+    
 }
 
 module.exports = BankAccount
--- Bank_Account.test.js
+++ Bank_Account.test.js
@@ -1,1 +1,8 @@
-const Account = require ('./Bank_Account.js')
+const BankAccount = require ('./Bank_Account.js')
+
+describe('The bank account should display a zero balance', () => {
+    it('When we call for the balance it should be zero', () => {
+        const account = new BankAccount()
+        expect(account.getBalance()).toBe(0);
+    })
+});
