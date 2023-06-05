fbd3d79038b569c34d29e70d24ebd3aa7ad26c5b
ShaunFlood <shaunflood@hotmail.co.uk>
2023-06-05 18:09:49 +0100

    Test now passed for updated test of withdrawl transaction limit

--- Bank_Account.js
+++ Bank_Account.js
@@ -12,6 +12,8 @@ class BankAccount {
     withdraw(number) {
         if (number <= 0 || number > this.balance)
             throw new Error('Invalid withdrawal amount') 
+        if (number >= 5001)
+            throw new Error('Your withdrawal limit is 5000 per transaction')
         this.balance -= number;
     }
 }
